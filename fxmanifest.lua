801d73c4d3600b42baec85c4abb6fc92da512955
Pope7015 <87500788+Pope7015@users.noreply.github.com>
2021-09-08 17:15:24 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Define the FX Server version and game type
+fx_version "adamant"
+game "gta5"
+
+-- Manifest
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Resource Info
+name 'Non-Emergency'
+description 'FiveM standalone /nonemergency command!'
+author 'Pope7015'
+
+server_script {
+	'server.lua',
+}
+
+-- Made By Pope7015
